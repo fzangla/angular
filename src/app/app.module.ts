db5fe8b8bcca6d175f11ad803a033bfaefd4a8a7
Franco Zangla Benitez <francomzangla@gmail.com>
2022-03-21 22:41:59 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TituloComponent } from './components/titulo/titulo.component';
+import { StudentsComponent } from './components/students/students.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TituloComponent,
+    StudentsComponent,
+    ToolbarComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
